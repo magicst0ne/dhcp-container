22c5a58049131ba81a456eb98492c1b8195151d8
magicst0ne <magicstone106@gmail.com>
2022-09-22 01:11:13 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 docker buildx build --platform=linux/amd64,linux/arm64,darwin/amd64,darwin/arm64 \
                     --progress=plain \
                     --no-cache \
-                    -t ninepeach/dhcp --push .
+                    -t ninepeach/dhcpd --push .
