6d7ee5a4118800ff5ad8bd49669b815952f411c4
magicstone <magicstone106@gmail.com>
2022-11-17 09:53:43 +0000

    log redirect to /dev/null

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp -u /dhcpd.conf /etc/dhcp/dhcpd.conf
 
-/usr/sbin/dhcpd -4 -f -d --no-pid -cf /etc/dhcp/dhcpd.conf
+/usr/sbin/dhcpd -4 -f -d --no-pid -cf /etc/dhcp/dhcpd.conf > /dev/null 2>&1
