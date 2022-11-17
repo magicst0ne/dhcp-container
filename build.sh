61e3f296e71c39d3a3925da9ef0f069deed7347e
ninepeach <4915416+ninepeach@users.noreply.github.com>
2022-11-17 15:39:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 #docker buildx build --platform=linux/amd64,linux/arm64,darwin/amd64,darwin/arm64 \
-docker buildx build --platform=linux/amd64,linux/arm64,darwin/amd64,darwin/arm64 \
-                    --progress=plain \
-                    --no-cache \
-                    -t ninepeach/dhcpd --push .
+#                    --progress=plain \
+#                    --no-cache \
+#                    -t ninepeach/dhcpd --push .
+
+buildctl build \
+--frontend dockerfile.v0 \
+--local context=. \
+--local dockerfile=. \
+--output type=image,name=docker.io/ninepeach/dhcpd,push=true
